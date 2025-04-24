ac57b9494b79288f8ca5f48e986eb903e83c3e2c
joshuamichael7 <josh@abrdn.co>
2025-04-24 04:14:04 +0000

    nav

--- src/components/MovieDetailPage.tsx
+++ src/components/MovieDetailPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import {
   ArrowLeft,
